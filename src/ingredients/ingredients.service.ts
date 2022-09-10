c19b49c63bd8e533cc79686a56408caa7186489f
João Melo <jvitor.ovidio2@gmail.com>
2022-09-10 14:29:08 -0300

    update eslint rule

--- src/ingredients/ingredients.service.ts
+++ src/ingredients/ingredients.service.ts
@@ -1,19 +1,29 @@
-import { Injectable } from '@nestjs/common';
-import { CreateIngredientDto } from './dto/create-ingredient.dto';
-import { UpdateIngredientDto } from './dto/update-ingredient.dto';
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { CreateIngredientDto } from "./dto/create-ingredient.dto";
+import { UpdateIngredientDto } from "./dto/update-ingredient.dto";
+import { Ingredient } from "./entities/ingredient.entity";
 
 @Injectable()
 export class IngredientsService {
+  constructor(
+    @InjectRepository(Ingredient)
+    private ingredientsRepository: Repository<Ingredient>
+  ) {}
+
   create(createIngredientDto: CreateIngredientDto) {
-    return 'This action adds a new ingredient';
+    return this.ingredientsRepository.save(createIngredientDto);
   }
 
   findAll() {
-    return `This action returns all ingredients`;
+    return this.ingredientsRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} ingredient`;
+  findOne(id: string) {
+    return this.ingredientsRepository.findOneBy({
+      id,
+    });
   }
 
   update(id: number, updateIngredientDto: UpdateIngredientDto) {
